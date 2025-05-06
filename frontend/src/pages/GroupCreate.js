2b4b4220194492392a58f6aba9dcb67b7fa54f0a
IT22605634 <aks.abeysekara@gmail.com>
2025-05-07 04:33:10 +0530

    heder fix

--- frontend/src/pages/GroupCreate.js
+++ frontend/src/pages/GroupCreate.js
@@ -5,7 +5,7 @@ import GroupForm from "../components/GroupForm";
 function GroupCreate() {
   return (
     <>
-      <Header />
+  
       <div className="min-h-screen bg-gray-50 py-8">
         <GroupForm />
       </div>
