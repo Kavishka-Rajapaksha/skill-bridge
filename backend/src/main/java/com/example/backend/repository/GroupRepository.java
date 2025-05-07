4957cd21079d8a99fcc290315967d4f7b9e50bb6
CharukaDilshan25 <147806106+CharukaDilshan25@users.noreply.github.com>
2025-05-08 01:15:26 +0530

    grop repo

--- backend/src/main/java/com/example/backend/repository/GroupRepository.java
+++ backend/src/main/java/com/example/backend/repository/GroupRepository.java
@@ -3,5 +3,10 @@ package com.example.backend.repository;
 import com.example.backend.model.Group;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.List;
+
 public interface GroupRepository extends MongoRepository<Group, String> {
+    List<Group> findByCreatedBy(String userId);
+
+    boolean existsByName(String name);
 }
