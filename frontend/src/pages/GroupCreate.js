20858ff60f8bca17f2e5ae8a7af14dd156275fef
CharukaDilshan25 <147806106+CharukaDilshan25@users.noreply.github.com>
2025-05-08 01:21:35 +0530

    group

---
+++ frontend/src/pages/GroupCreate.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Header from "../components/Header";
+import GroupForm from "../components/GroupForm";
+
+function GroupCreate() {
+  return (
+    <>
+  
+      <div className="min-h-screen bg-gray-50 py-8">
+        <GroupForm />
+      </div>
+    </>
+  );
+}
+
+export default GroupCreate;
