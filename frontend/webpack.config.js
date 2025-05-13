5a2aeae7c9a0fe40df4ababb99041709ba562d72
IT22605634 <aks.abeysekara@gmail.com>
2025-05-14 04:34:47 +0530

    host

---
+++ frontend/webpack.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  devServer: {
+    allowedHosts: ['localhost', '.localhost', '127.0.0.1'],
+    host: '165.232.179.196',
+    port: 3000,
+    historyApiFallback: true,
+    hot: true
+  }
+};
