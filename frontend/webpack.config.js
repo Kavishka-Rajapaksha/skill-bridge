9ee7e145b4ead1d1facbff0f57d8e0ab7b36b271
IT22605634 <aks.abeysekara@gmail.com>
2025-05-14 05:36:59 +0530

    Update webpack.config.js

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,8 +1,11 @@
+const path = require('path');
+require('dotenv').config();
+
 module.exports = {
   devServer: {
-    allowedHosts: ['localhost', '.localhost', '127.0.0.1'],
-    host: '165.232.179.196',
-    port: 3000,
+    allowedHosts: ['localhost', '.localhost', '127.0.0.1', process.env.HOST],
+    host: process.env.HOST || 'localhost',
+    port: process.env.PORT || 3000,
     historyApiFallback: true,
     hot: true
   }
